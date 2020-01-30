81147f67d5e874c0ee74b7cf57a592cfc4b8e113
alexoprescu25 <58701251+alexoprescu25@users.noreply.github.com>
2020-01-30 23:26:50 +0200

    Add files via upload

--- main.js
+++ main.js
@@ -1,53 +1,39 @@
-function display() {  
-    for(let i = 0; i <= 20; i++) {
-        console.log(i);
-    }
-    for(let j = 0; j <= 20; j++) {
-        if(j % 2) {
-            console.log(j);
-            
-        }
-    }
+function play() {
+
+    
+const arr = ["rock", "paper", "scissors"];
+const random = Math.floor(Math.random() * arr.length);
+const random1 = Math.floor(Math.random() * arr.length);
+
+const user = arr[random];
+const computer = arr[random1];
+  
+console.log("Computer choice: ", computer);
+console.log("User choice: ", user); 
+  
+if(user == "rock" && computer == "rock") {
+  console.log("Equal!");
+} else if(user == "rock" && computer == "paper") {
+  console.log("Computer wins!");
+} else if(user == "rock" && computer == "scissors") {
+  console.log("User wins!");
+} else if(user == "paper" && computer == "rock") {
+  console.log("User wins!");
+} else if(user == "paper" && computer == "paper") {
+  console.log("Equal!");
+} else if(user == "paper" && computer == "scissors") {
+  console.log("Computer wins!");
+} else if(user == "scissors" && computer == "rock") {
+  console.log("Computer wins!");
+} else if(user == "scissors" && computer == "paper") {
+  console.log("User wins!");
+} else if(user == "scissors" && computer == "scissors") {
+  console.log("Equal!");
 }
 
-display();
 
-function sum() {
-    let arr = [1, 2, 3, 4, 5, 6, 7];
-    let str = 0;
-    for(let i = arr.length - 1; i >= 0; i-- ) {
-        str = str + arr[i];
-    }
-    console.log('Suma elementelor este: ', str);
 }
 
-sum();
+const buttonInc = document.querySelector('.js-play');
+buttonInc.addEventListener('click', play);
 
-function max() {
-    let arr = [20, 11, 43, 2, 18]
-    let max = -Infinity;
-    for(let i = arr.length - 1; i >= 0; i--) {
-    if(max < arr[i]) {
-        max = arr[i];
-    }
-}
-       
-    console.log('Maximul elementelor este: ', max);
-}
-
-max();
-
- 
-function count() {
-    let arr = [1, 2, 1, 3, 25, 1, 4, 7];
-    let value = 1;
-    let nr = 0;
-    for(let i = 0; i < arr.length; i++) {
-        if(arr[i] == value) {
-            nr++;
-        }
-    }
-    console.log('Numarul 1 apare de ' + nr + ' ori');
-}
-
-count();
