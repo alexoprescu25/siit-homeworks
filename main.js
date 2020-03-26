7f81a3bca1d9e35a2fe7531ef9132162e936073e
Alexandru Oprescu <alexandru.oprescu@outlook.com>
2020-03-26 13:48:08 +0200

    geolocation

--- main.js
+++ main.js
@@ -1,53 +1,27 @@
-function display() {  
-    for(let i = 0; i <= 20; i++) {
-        console.log(i);
-    }
-    for(let j = 0; j <= 20; j++) {
-        if(j % 2) {
-            console.log(j);
-            
+(function() {
+    navigator.geolocation.getCurrentPosition(getCoordinates, console.warn);
+
+    function getCoordinates(data) {
+        const latitude = data.coords.latitude;
+        const longitude = data.coords.longitude;      
+        
+        const url = `http://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=696dd88412b6e63a0604de6c8b0139cf`;
+
+        fetch(url)
+            .then(res => res.json())
+            .then(display);
+
+        function display(data) {
+            const weather = document.querySelector('.js-weather');
+            const description = document.querySelector('.js-description');
+            const temp = document.querySelector('.js-temp');
+
+            weather.innerHTML = 'Weather in ' + data.name + ':';
+            description.innerHTML = data.weather[0].main + ', ' + data.weather[0].description;
+            temp.innerHTML = 'Temperature: ' + (data.main.temp - 273.15).toFixed(1) + ' &#8451;';
+
+            document.body.append(weather, description, temp);
         }
+        
     }
-}
-
-display();
-
-function sum() {
-    let arr = [1, 2, 3, 4, 5, 6, 7];
-    let str = 0;
-    for(let i = arr.length - 1; i >= 0; i-- ) {
-        str = str + arr[i];
-    }
-    console.log('Suma elementelor este: ', str);
-}
-
-sum();
-
-function max() {
-    let arr = [20, 11, 43, 2, 18]
-    let max = -Infinity;
-    for(let i = arr.length - 1; i >= 0; i--) {
-    if(max < arr[i]) {
-        max = arr[i];
-    }
-}
-       
-    console.log('Maximul elementelor este: ', max);
-}
-
-max();
-
- 
-function count() {
-    let arr = [1, 2, 1, 3, 25, 1, 4, 7];
-    let value = 1;
-    let nr = 0;
-    for(let i = 0; i < arr.length; i++) {
-        if(arr[i] == value) {
-            nr++;
-        }
-    }
-    console.log('Numarul 1 apare de ' + nr + ' ori');
-}
-
-count();
+})();
